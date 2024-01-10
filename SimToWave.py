9300a6c7254fed8b38280d442ee5999337b1d5a2
gourmetwater <110051910+gourmetwater@users.noreply.github.com>
2024-01-10 16:08:01 -0500

    Update SimToWave.py

--- SimToWave.py
+++ SimToWave.py
@@ -1,9 +1,12 @@
 import numpy as np
 import sys
+import os
 import wavio
 from csvsifter import siftcsv
 
 filename = sys.argv[1]
+outputname = os.path.splitext(filename)[0] + '.wav'
+
 data, duration = siftcsv(dataname=filename)
 base = np.copy(data)
 
@@ -13,4 +16,4 @@ T = 3
 for n in range(1,(int(3/duration)+1)):
     data = np.concatenate((data,base),axis = 0)
 
-wavio.write("Vout.wav",data,rate,sampwidth = T)
+wavio.write(outputname,data,rate,sampwidth = T,scale="auto")
